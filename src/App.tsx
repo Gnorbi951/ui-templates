3151ee1106f6b581f00b7362e0813545de6fb675
Norbert Gocze <gnorbi951@gmail.com>
2021-04-02 17:42:49 +0200

    Add particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -6,7 +7,7 @@ const App = () => {
 
   return (
     <>
-        <p>My app ;)</p>
+        My app
     </>
   )
 
