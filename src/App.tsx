699ff4ee0458a87ef2d9846b794f531a1582ef58
Norbert Gocze <gnorbi951@gmail.com>
2021-04-02 21:04:24 +0200

    Integrate mainpage to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Router, Route, Switch} from "react-router-dom";
+import { createBrowserHistory } from 'history';
+
+
+import MainPage from "./Components/MainPage/MainPage";
 import './App.css';
 
+
 const App = () => {
-
-
-  return (
-    <>
-        My app
-    </>
-  )
+    const history = createBrowserHistory();
+  
+    return (
+        <>
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/" component={MainPage} />
+                </Switch>
+            </Router>
+        </>
+    )
 
 }
 
